ad1c08541d8d6e2feaf10932e045d652b76c2135
Rybin Andrey <rybin.ag@gmail.com>
2020-11-24 11:12:36 +0300

    some

---
+++ src/App.js
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">        
+        <p>
+          Hi, I'm React.
+        </p>
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
